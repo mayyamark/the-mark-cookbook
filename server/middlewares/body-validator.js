52d294704dde989b4946904c73e94c37c6b97bb7
Mayya Markova <mayya.markova@gmail.com>
2020-12-06 12:19:59 +0200

    #3 #7 #9 Add body validator middleware

---
+++ server/middlewares/body-validator.js
@@ -0,0 +1,18 @@
+const bodyValidator = (schema) => {
+  return (req, res, next) => {
+    const body = req.body;
+    const validations = Object.keys(schema);
+
+    const fails = validations
+      .map((v) => schema[v](body[v]))
+      .filter((e) => e !== null);
+
+    if (fails.length > 0) {
+      res.status(400).send(fails);
+    } else {
+      next();
+    }
+  };
+};
+
+export default bodyValidator;
