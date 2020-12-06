0f948848063e6a59b217ca04e1018b4c71bb748d
Mayya Markova <mayya.markova@gmail.com>
2020-12-06 12:05:04 +0200

    #1 Add search by query params

--- server/data/recipes.data.js
+++ server/data/recipes.data.js
@@ -24,7 +24,36 @@ const getAll = async () => {
   return { available: availableRecipesData, deleted: deletedRecipesData };
 };
 
+const searchBy = async (
+  name = undefined,
+  category = undefined,
+  order = undefined,
+) => {
+  let recipesSql = `
+      SELECT r.recipeID AS 'recipeID', r.name AS 'recipeName', t.category, r.date AS 'addDate', isDeleted
+      FROM recipes r
+      JOIN categories t ON r.categoryID = t.categoryID
+  `;
+
+  if (name & category) {
+    recipesSql += `WHERE r.name LIKE '%${name}% AND t.category LIKE '${category}' `;
+  } else if (name) {
+    recipesSql += `WHERE r.name LIKE '%${name}%' `;
+  } else if (category) {
+    recipesSql += `WHERE t.category LIKE '${category}' `;
+  }
+
+  if (order) {
+    recipesSql += `ORDER BY r.name ${order};`;
+  } else {
+    recipesSql += 'ORDER BY r.recipeID ASC;';
+  }
+
+  return await pool.query(recipesSql);
+};
+
 
 export default {
   getAll,
+  searchBy,
 };
